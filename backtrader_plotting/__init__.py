60c6d5cd4275e3ac660a78a2e929b0ce97dbe274
Clemens Sageder <77528379+Sageder@users.noreply.github.com>
2026-02-07 14:48:53 +0000

    fix

--- backtrader_plotting/__init__.py
+++ backtrader_plotting/__init__.py
@@ -1,3 +1,12 @@
+# Fix numpy compatibility issues with newer versions
+import numpy as np
+if not hasattr(np, 'bool8'):
+    np.bool8 = np.bool_
+if not hasattr(np, 'int0'):
+    np.int0 = np.int_
+if not hasattr(np, 'float_'):
+    np.float_ = np.float64
+
 from backtrader_plotting.bokeh.bokeh import Bokeh
 
 from backtrader_plotting.bokeh.optbrowser import OptBrowser
